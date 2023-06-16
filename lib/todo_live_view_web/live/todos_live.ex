62be57521b7e2dae58db6606f60c97630e5c9f06
Kaelyn <kaelynhyson@yahoo.com>
2023-06-15 18:58:19 -0700

    Pushing up

---
+++ lib/todo_live_view_web/live/todos_live.ex
@@ -0,0 +1,47 @@
+defmodule TodoLiveViewWeb.TodosLive do
+  use TodoLiveViewWeb, :live_view
+  alias TodoLiveView.Todos
+  alias TodoLiveView.Todos.Todo
+
+
+    @todos_topic "todos"
+def mount(_params, _session, socket) do
+  if connected?(socket), do: TodoLiveViewWeb.Endpoint.subscribe(@todos_topic)
+
+  {:ok,
+   assign(socket, items: Todos.list_todos())
+   |> assign_todo()}
+end
+
+def handle_params(params, _url, socket) do
+  items = Todos.list_todos()
+
+  case params["filter_by"] do
+    "completed" ->
+      completed_items = Enum.filter(items, &(&1.completed == true))
+      {:noreply, assign(socket, items: completed_items)}
+
+    "active" ->
+      active_items = Enum.filter(items, &(&1.completed == false))
+      {:noreply, assign(socket, items: active_items)}
+
+    _ ->
+      {:noreply, assign(socket, items: items)}
+  end
+end
+
+def handle_info(%{event: "todos_updated", payload: %{items: items}}, socket) do
+  {:noreply, assign(socket, items: items)}
+end
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     assign(socket, items: Todos.list_todos())
+     |> assign_todo()}
+  end
+
+  def assign_todo(socket) do
+    socket
+    |> assign(:todo, %Todo{})
+  end
+end
